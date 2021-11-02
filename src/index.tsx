119783d3b3e6988718c595e8b71a2ebbf9f4aa27
Henrique Gomes <hgs@icomp.ufam.edu.br>
2021-11-02 02:03:49 -0400

    [Feat] Added routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Routes } from "./Components/Routes";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
