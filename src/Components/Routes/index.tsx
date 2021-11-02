03a94d2ada0f81517d65f40bbcd0d67005b6c058
Henrique Gomes <hgs@icomp.ufam.edu.br>
2021-11-02 01:45:53 -0400

    [Fix] Change Hash to Browser router

--- src/Components/Routes/index.tsx
+++ src/Components/Routes/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { ROUTES } from "../../Config/routes";
 import { HomePage } from "../../Pages/Home";
 
 export const Routes: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route path={ROUTES.HOME} component={HomePage} />
         <Redirect from="*" to={ROUTES.HOME} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
